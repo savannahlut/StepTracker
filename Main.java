b3a1d17924943d0448c6659b3c7b6b8ede6b665b
Savannah-Lu <savannahlut@nycstudents.net>
2025-03-27 17:25:48 +0000

    First Draft

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        
+        StepTracker tr = new StepTracker(10000);
+        System.out.println(tr.activeDays());
     }
 }
--- StepTracker.java
+++ StepTracker.java
@@ -1,8 +1,30 @@
 public class StepTracker{
     final int minSteps;
-    int days;
+    int totalSteps;
+    int activeDays;
+    int totalDays;
 
     public StepTracker(int minSteps){
         this.minSteps = minSteps;
+        totalSteps = 0;
+        activeDays = 0; 
+        totalDays = 0;
+    }
+
+    public int activeDays(){
+        return activeDays;
+    }
+
+    public void addDailySteps(int steps){
+        if(steps > minSteps){
+            activeDays++;
+        }
+        totalSteps += steps;
+        totalDays++;
+    } 
+
+    public double averageSteps(){
+        if(totalSteps = 0) return 0; 
+        return (totalSteps / totalDays);
     }
 }
